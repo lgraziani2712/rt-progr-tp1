2c4c842d1a024bf6e5196a23fbeb2492d098a593
Luciano Graziani <lgraziani2712@gmail.com>
2025-09-22 16:40:27 -0300

    enemy: add tiny animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 3;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Projectile"))
         {
             health--;
+            animator.SetTrigger("BeingAttacked");
 
             if (health == 0)
             {
